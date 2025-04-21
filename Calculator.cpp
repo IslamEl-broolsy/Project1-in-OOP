741bd3cdaa2646abca140e9bb9383cc64739fd05
Islam_EL-broolsy <156857284+IslamEl-broolsy@users.noreply.github.com>
2025-04-21 23:07:11 +0200

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+class ClsCalculator
+{
+private:
+
+	int _Num = 0;
+	char _OPType;
+	int _AddNum;
+	int _SubNum;
+	int _DivNum;
+	int _MultNum;
+
+public:
+
+	void PrintResult()
+	{
+		switch (_OPType)
+		{
+
+		case '+':
+			cout << "Result After Adding " << _AddNum << " is : " << _Num << endl;
+			break;
+
+		case '-':
+			cout << "Result After Subtracting " << _SubNum << " is : " << _Num << endl;
+			break;
+
+		case '*':
+			cout << "Result After Multiplying " << _MultNum << " is : " << _Num << endl;
+			break;
+
+
+		case '/':
+			cout << "Result After Dividing " << _DivNum << " is : " << _Num << endl;
+			break;
+
+		default:
+			cout << "Result After Clear " << _Num << " is : " << _Num << endl;
+			break;
+		}
+	}
+
+	int Add(int AddNum)
+	{
+		_OPType = '+';
+		_AddNum = AddNum;
+		return _Num += AddNum;
+	}
+
+	int Subtract(int SubNum)
+	{
+		_OPType = '-';
+		_SubNum = SubNum;
+		return _Num -= SubNum;
+	}
+
+	int Divide(int DivNum)
+	{
+		_OPType = '/';
+
+		if (DivNum == 0)
+		{
+			DivNum = 1;
+			_DivNum = DivNum;
+			return _Num /= DivNum;
+		}
+		else
+		{
+			_DivNum = DivNum;
+			return _Num /= DivNum;
+		}
+	}
+
+	int Multiply(int MultNum)
+	{
+		_OPType = '*';
+		_MultNum = MultNum;
+		return _Num *= MultNum;
+	}
+
+	int Clear()
+	{
+		_OPType = ' ';
+		return _Num = 0;
+	}
+};
+
+int main()
+{
+	ClsCalculator Calculator1;
+
+	Calculator1.Clear();
+
+	Calculator1.Add(100);
+	Calculator1.PrintResult();
+
+	Calculator1.Subtract(20);
+	Calculator1.PrintResult();
+
+	Calculator1.Multiply(3);
+	Calculator1.PrintResult();
+
+	Calculator1.Divide(2);
+	Calculator1.PrintResult();
+
+	Calculator1.Divide(0);
+	Calculator1.PrintResult();
+
+	Calculator1.Clear();
+	Calculator1.PrintResult();
+
+	system("pause > 0");
+}
